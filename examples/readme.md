6fa57bd541779a7c819f72b37f9d26649b5c6454
Jonas Thuvesson Winberg <jonas.winberg@silobreaker.com>
2021-04-12 22:52:48 +0200

    init

---
+++ examples/readme.md
@@ -0,0 +1,12 @@
+# Vite Elm Starter
+
+A base project for [Elm](https://elm-lang.org/), using vite and [vite-plugin-elm](https://www.npmjs.com/package/vite-plugin-elm)!
+
+## Includes
+
+- [Elm](https://elm-lang.org/) support
+- [SCSS](https://sass-lang.com/) support
+- [Tailwind](https://tailwindcss.com/)
+- HMR in development
+
+More about [Vite](https://vitejs.dev/)
---
+++ examples/src/Hello.elm
@@ -0,0 +1,52 @@
+module Hello exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+type alias Model = Int
+
+init : Model
+init =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+    Reset ->
+      0
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [ class "main"]
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "reset counter"]
+    ]
